c1eedcad81d991d997a25b731cbb112e82dee4cf
milos.smiljanic <milos.smiljanic@comtrade.com>
2018-01-27 19:14:38 +0100

    redundant code removed

--- Assets/Scripts/SwitchHandler.cs
+++ Assets/Scripts/SwitchHandler.cs
@@ -31,22 +31,12 @@ public class SwitchHandler : MonoBehaviour {
 
     void OnMouseOver()
     {
-        //this.gameObject.GetComponent<SpriteRenderer>().sprite = switchHighlight;
-
         objectHighlight.GetComponent<SpriteRenderer>().color = Color.white;
-        //this.gameObject.GetComponent<SpriteRenderer>().color = Color.cyan; // RGBA(107.000, 33.000, 152.000, 1.000);
-        //The mouse is no longer hovering over the GameObject so output this message each frame
-        Debug.Log("Mouse is on GameObject.");
-        //this.gameObject.GetComponent<SpriteRenderer>().sprite = getNextSprite();
     }
 
     void OnMouseExit()
     {
         objectHighlight.GetComponent<SpriteRenderer>().color = Color.clear;
-
-        //this.gameObject.GetComponent<SpriteRenderer>().color = this.gameObject.GetComponent<SpriteRenderer>().color;
-        //The mouse is no longer hovering over the GameObject so output this message each frame
-        Debug.Log("Mouse is no longer on GameObject.");
     }
 
     Sprite getNextSprite()
